d33bc744225e9ce25b3ef204b76fb14c0f82248c
Israel Abner <133179040+Abner856@users.noreply.github.com>
2026-05-31 10:32:40 -0300

    Criar uma classe Quarto para gerenciamento de quartos de hotel

---
+++ Quarto.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hotel;
+
+/**
+ *
+ * @author Israel
+ */
+public class Quarto {
+
+    private String idNumero;
+    private int capacidade;
+    private String tipo;
+
+    public Quarto(String idNumero, int capacidade, String tipo) {
+        this.idNumero = idNumero;
+        this.capacidade = capacidade;
+        this.tipo = tipo;
+    }
+}
