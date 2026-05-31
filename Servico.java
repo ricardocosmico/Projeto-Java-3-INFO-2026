2d4d4001fd4013b8d0c295e865e27948a51dadb4
Israel Abner <133179040+Abner856@users.noreply.github.com>
2026-05-31 10:35:03 -0300

    Crie uma classe de serviço com atributos de ID e valor.

---
+++ Servico.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hotel;
+
+/**
+ *
+ * @author Israel
+ */
+public class Servico {
+
+    private String idServico;
+    private double valorServico;
+
+    public Servico(String idServico, double valorServico) {
+        this.idServico = idServico;
+        this.valorServico = valorServico;
+    }
+}
