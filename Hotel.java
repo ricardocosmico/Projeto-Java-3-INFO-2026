b3ddc3c56b61779369517a45c67388e39dbd6315
vinimr1 <viniciusmorais1541@icloud.com>
2026-05-31 12:04:59 -0300

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,3 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listadeestudo;
+
+/**
+ *
+ * @author vinic
+ */
 public class Hotel {
+      int  id;
+    String nome;
+    String endereco;
+    String siteHospedagem;
+
+    public Hotel(int id, String nome, String endereco, String siteHospedagem) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.siteHospedagem = siteHospedagem;
+    }
+    
+    
 
 }
